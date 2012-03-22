19500f462bff5c2aeef77a0ffc64ad6a9c547f09
Mark Otto <markotto@twitter.com>
2012-03-22 00:28:03 -0700

    remove comment, fix broken meta tag

--- wp-content/themes/bootstrap-blog/header.php
+++ wp-content/themes/bootstrap-blog/header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
   <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width; initial-scale=1.0, maximum-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title><?php wp_title('&middot;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 
