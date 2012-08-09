43675d56330441d0860d71b7977775f0ca04df07
Mark Otto <markotto@twitter.com>
2012-08-09 11:12:36 -0700

    nuke wordpress

--- wp-content/themes/bootstrap-blog/single.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-
-  <p><a href="<?php bloginfo('siteurl');?>" title="Back home">&larr; Back home</a></p>
-
-	<?php get_template_part('loop', 'index'); ?>
-  
-<?php get_footer(); ?>
