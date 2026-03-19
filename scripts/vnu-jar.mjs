60456c8ca205d7f77a3437218a8fdf5c39b0717a
XhmikosR <xhmikosr@gmail.com>
2026-03-19 07:49:56 +0200

    Update vnu-jar.mjs

--- scripts/vnu-jar.mjs
+++ scripts/vnu-jar.mjs
@@ -18,7 +18,7 @@ execFile('java', ['-version'], (error, _stdout, stderr) => {
 
   console.log('Running vnu-jar validation...')
 
-  const is32bitJava = !stderr.includes('64-Bit')
+  const is32bitJava = !/64-Bit/.test(stderr)
 
   // vnu-jar accepts multiple ignores joined with a `|`.
   // Also note that the ignores are string regular expressions.
